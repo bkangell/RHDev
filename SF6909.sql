de54d2db32a4ad30829b824af7bd135c78a6d8d2
Brenda Kathleen Angell <bangell@remarkablehealth.com>
2018-09-12 16:27:29 -0700

    Update SF6909.sql

--- SF6909.sql
+++ SF6909.sql
@@ -1,4 +1,4 @@
-/* The script is created for the ticket #SF6909 to Update [dbo]. Client */
+/* The script is created for the ticket #SF6909ba20180912 to Update [dbo]. Client */
 DECLARE @currDateTime          VARCHAR(30);
 DECLARE @sqlStr1               VARCHAR(max);
 SET NOCOUNT ON;
@@ -48,92 +48,96 @@ BEGIN
 
 /*  backup of tables */
 
-	SELECT * INTO   [bak].SF6909_TRPLog	FROM [dbo].	TRPLog WHERE CientKey=92496 and id in
+	SELECT * INTO   [bak].SF6909ba20180912_TRPLog	FROM [dbo].	TRPLog WHERE ClientKey=92496 and id in
 					 (select ID from TRPLog t inner join ClientPacket p on p.ID=t.PacketKey
 						  where t.ClientKey=92496 and EnrollmentKey=688108 )
-	SELECT * INTO   [bak].SF6909_ClientAccessLog	FROM [dbo].	ClientAccessLog	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientPacket	FROM [dbo].	ClientPacket	 WHERE CientKey=92496 and ClientEnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientICD	FROM [dbo].	ClientICD	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientPacketForms	FROM [dbo].	ClientPacketForms	 WHERE CientKey=92496 and ClientEnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientPacketCommonDx2015MigrationHistory	FROM [dbo].	ClientPacketCommonDx2015MigrationHistory	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_PNChartQuestionResponseMaster	FROM [dbo].	PNChartQuestionResponseMaster	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientProgressNote	FROM [dbo].	ClientProgressNote	 WHERE CientKey=92496 and ClientEnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_Alerts	FROM [dbo].	Alerts	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_SignedForms	FROM [dbo].	SignedForms	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ReleaseOfInformationToCategories	FROM [dbo].	ReleaseOfInformationToCategories	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientPacketCommon	FROM [dbo].	ClientPacketCommon	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_MailMasterAttachments	FROM [dbo].	MailMasterAttachments	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientStatus	FROM [dbo].	ClientStatus	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ReleaseOfInformationFromCategories	FROM [dbo].	ReleaseOfInformationFromCategories	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientISP	FROM [dbo].	ClientISP	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_MMScripts	FROM [dbo].	MMScripts	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientCurrentData	FROM [dbo].	ClientCurrentData	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_AppointmentHistory	FROM [dbo].	AppointmentHistory	 WHERE CientKey=92496 --and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ProgNoteValeoNew	FROM [dbo].	ProgNoteValeoNew	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_Appointments	FROM [dbo].	Appointments	 WHERE CientKey=92496 and AppointmentDate>'2018-09-01'
-	SELECT * INTO   [bak].SF6909_Claims	FROM [dbo].	Claims	 WHERE CientKey=92496 and ServiceStartDate>'2018-09-01'
-	SELECT * INTO   [bak].SF6909_ClientProgram	FROM [dbo].	ClientProgram	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientContacts	FROM [dbo].	ClientContacts	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientProgramReferral	FROM [dbo].	ClientProgramReferral	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientProgramMain	FROM [dbo].	ClientProgramMain	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientAddresses	FROM [dbo].	ClientAddresses	 WHERE CientKey=92496 and TransactionDate>'2018-09-01'
-	SELECT * INTO   [bak].SF6909_Referral	FROM [dbo].	Referral	 WHERE CientKey=92496 and DateOfReferral>'2018-09-01'
-	SELECT * INTO   [bak].SF6909_ClientEnrollment	FROM [dbo].	ClientEnrollment	 WHERE CientKey=92496 and ID=688108 
-	SELECT * INTO   [bak].SF6909_SMMHCInventoryLog	FROM [dbo].	SMMHCInventoryLog	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ScoreTrakClientTargetMaster	FROM [dbo].	ScoreTrakClientTargetMaster	 WHERE CientKey=92496 and CreateDateTime>'2018-09-01'
-	SELECT * INTO   [bak].SF6909_ClientStatusKids	FROM [dbo].	ClientStatusKids	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_Closure	FROM [dbo].	Closure	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_DiagnosisUpdate [dbo].	FROM [dbo].	DiagnosisUpdate [dbo].	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_PNChartMaster	FROM [dbo].	PNChartMaster	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientComms	FROM [dbo].	ClientComms	 WHERE CientKey=92496 
-	SELECT * INTO   [bak].SF6909_ValeoInHouseCSCRef	FROM [dbo].	ValeoInHouseCSCRef	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ClientPacketFormSignature	FROM [dbo].	ClientPacketFormSignature	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ProgNoteValeoCrisis	FROM [dbo].	ProgNoteValeoCrisis	 WHERE CientKey=92496 and EnrollmentKey=688108 
-	SELECT * INTO   [bak].SF6909_ValeoMasterPSRef	FROM [dbo].	ValeoMasterPSRef	 WHERE CientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientAccessLog	FROM [dbo].	ClientAccessLog	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientPacket	FROM [dbo].	ClientPacket	 WHERE ClientKey=92496 and ClientEnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientICD	FROM [dbo].	ClientICD	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientPacketForms	FROM [dbo].	ClientPacketForms	 WHERE ClientKey=92496 and ClientEnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientPacketCommonDx2015MigrationHistory	FROM [dbo].	ClientPacketCommonDx2015MigrationHistory	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_PNChartQuestionResponseMaster	FROM [dbo].	PNChartQuestionResponseMaster	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientProgressNote	FROM [dbo].	ClientProgressNote	 WHERE ClientKey=92496 and ClientEnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_Alerts	FROM [dbo].	Alerts	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_SignedForms	FROM [dbo].	SignedForms	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ReleaseOfInformationToCategories	FROM [dbo].	ReleaseOfInformationToCategories	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientPacketCommon	FROM [dbo].	ClientPacketCommon	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_MailMasterAttachments	FROM [dbo].	MailMasterAttachments	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientStatus	FROM [dbo].	ClientStatus	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ReleaseOfInformationFromCategories	FROM [dbo].	ReleaseOfInformationFromCategories	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientISP	FROM [dbo].	ClientISP	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_MMScripts	FROM [dbo].	MMScripts	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientCurrentData	FROM [dbo].	ClientCurrentData	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_AppointmentHistory	FROM [dbo].	AppointmentHistory	 WHERE ClientKey=92496 --and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ProgNoteValeoNew	FROM [dbo].	ProgNoteValeoNew	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_Appointments	FROM [dbo].	Appointments	 WHERE ClientKey=92496 and AppointmentDate>'2018-09-01'
+	SELECT * INTO   [bak].SF6909ba20180912_Claims	FROM [dbo].	Claims	 WHERE ClientKey=92496 and ServiceStartDate>'2018-09-01'
+	SELECT * INTO   [bak].SF6909ba20180912_ClientProgram	FROM [dbo].	ClientProgram	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientContacts	FROM [dbo].	ClientContacts	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientProgramReferral	FROM [dbo].	ClientProgramReferral	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientProgramMain	FROM [dbo].	ClientProgramMain	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientAddresses	FROM [dbo].	ClientAddresses	 WHERE ClientKey=92496 and TransactionDate>'2018-09-01'
+	SELECT * INTO   [bak].SF6909ba20180912_Referral	FROM [dbo].	Referral	 WHERE ClientKey=92496 and DateOfReferral>'2018-09-01'
+	SELECT * INTO   [bak].SF6909ba20180912_ClientEnrollment	FROM [dbo].	ClientEnrollment	 WHERE ClientKey=92496 and ID=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_SMMHCInventoryLog	FROM [dbo].	SMMHCInventoryLog	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ScoreTrakClientTargetMaster	FROM [dbo].	ScoreTrakClientTargetMaster	 WHERE ClientKey=92496 and CreateDateTime>'2018-09-01'
+	SELECT * INTO   [bak].SF6909ba20180912_ClientStatusKids	FROM [dbo].	ClientStatusKids	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_Closure	FROM [dbo].	Closure	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_DiagnosisUpdate [dbo].	FROM [dbo].	DiagnosisUpdate [dbo].	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_PNChartMaster	FROM [dbo].	PNChartMaster	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientComms	FROM [dbo].	ClientComms	 WHERE ClientKey=92496 
+	SELECT * INTO   [bak].SF6909ba20180912_ValeoInHouseCSCRef	FROM [dbo].	ValeoInHouseCSCRef	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ClientPacketFormSignature	FROM [dbo].	ClientPacketFormSignature	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ProgNoteValeoCrisis	FROM [dbo].	ProgNoteValeoCrisis	 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	SELECT * INTO   [bak].SF6909ba20180912_ValeoMasterPSRef	FROM [dbo].	ValeoMasterPSRef	 WHERE ClientKey=92496 and EnrollmentKey=688108 
 
 	/* ----------------------------------------------Update [dbo]. REPLACE ClientKey 92496 with 100000 */
-	Update [dbo].	TRPLog	SET ClientKey=100000 WHERE CientKey=92496 and id in 
+	Update [dbo].TRPLog	SET ClientKey=100000 WHERE ClientKey=92496 and id in 
 		(select ID from TRPLog t inner join ClientPacket p on p.ID=t.PacketKey
-			where t.ClientKey=92496 and EnrollmentKey=688108)
-	Update [dbo].ClientAccessLog	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientPacket	SET ClientKey=100000 WHERE CientKey=92496 and ClientEnrollmentKey=688108 
-	Update [dbo].ClientICD	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientPacketForms	SET ClientKey=100000 WHERE CientKey=92496 and ClientEnrollmentKey=688108 
-	Update [dbo].ClientPacketCommonDx2015MigrationHistory	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].PNChartQuestionResponseMaster	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientProgressNote	SET ClientKey=100000 WHERE CientKey=92496 and ClientEnrollmentKey=688108 
-	Update [dbo].Alerts	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].SignedForms	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ReleaseOfInformationToCategories	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientPacketCommon	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].MailMasterAttachments	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientStatus	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ReleaseOfInformationFromCategories	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientISP	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].MMScripts	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientCurrentData	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].AppointmentHistory	SET ClientKey=100000 WHERE CientKey=92496 --and EnrollmentKey=688108 
-	Update [dbo].ProgNoteValeoNew	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].Appointments	SET ClientKey=100000 WHERE CientKey=92496 and AppointmentDate>'2018-09-01'
-	Update [dbo].Claims	SET ClientKey=100000 WHERE CientKey=92496 and ServiceStartDate>'2018-09-01'
-	Update [dbo].ClientProgram	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientContacts	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientProgramReferral	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientProgramMain	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientAddresses	SET ClientKey=100000 WHERE CientKey=92496 and TransactionDate>'2018-09-01'
-	Update [dbo].Referral	SET ClientKey=100000 WHERE CientKey=92496 and DateOfReferral>'2018-09-01'
-	Update [dbo].ClientEnrollment	SET ClientKey=100000 WHERE CientKey=92496 and ID=688108 
-	Update [dbo].SMMHCInventoryLog	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ScoreTrakClientTargetMaster	SET ClientKey=100000 WHERE CientKey=92496 and CreateDateTime>'2018-09-01'
-	Update [dbo].ClientStatusKids	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].Closure	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].DiagnosisUpdate [dbo].	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].PNChartMaster	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientComms	SET ClientKey=100000 WHERE CientKey=92496 
-	Update [dbo].ValeoInHouseCSCRef	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ClientPacketFormSignature	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ProgNoteValeoCrisis	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
-	Update [dbo].ValeoMasterPSRef	SET ClientKey=100000 WHERE CientKey=92496 and EnrollmentKey=688108 
+			where t.ClientKey=92496 and ClientEnrollmentKey=688108)
+	Update [dbo].ClientAccessLog	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientPacket	SET ClientKey=100000 WHERE ClientKey=92496 and ClientEnrollmentKey=688108 
+	Update [dbo].ClientICD	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientPacketForms	SET ClientKey=100000 WHERE ClientKey=92496 and ClientEnrollmentKey=688108 
+	Update [dbo].ClientPacketCommonDx2015MigrationHistory	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].PNChartQuestionResponseMaster	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientProgressNote	SET ClientKey=100000 WHERE ClientKey=92496 and ClientEnrollment=688108 
+	Update [dbo].Alerts	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].SignedForms	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ReleaseOfInformationToCategories	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientPacketCommon	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].MailMasterAttachments	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientStatus	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ReleaseOfInformationFromCategories	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientISP	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].MMScripts	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientCurrentData	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].AppointmentHistory	SET ClientKey=100000 WHERE ClientKey=92496 and AppointmentDate>'2018-09-01'
+	Update [dbo].ProgNoteValeoNew	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].Appointments	SET ClientKey=100000 WHERE ClientKey=92496 and AppointmentDate>'2018-09-01'
+	Update [dbo].Claims	SET ClientKey=100000 WHERE ClientKey=92496 and ServiceStartDate>'2018-09-01'
+	Update [dbo].ClientProgram	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientContacts	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientProgramReferral	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientProgramMain	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientAddresses	SET ClientKey=100000 WHERE ClientKey=92496 and TransactionDate>'2018-09-01'
+	Update [dbo].Referral	SET ClientKey=100000 WHERE ClientKey=92496 and DateOfReferral>'2018-09-01'
+	Update [dbo].ClientEnrollment	SET ClientKey=100000 WHERE ClientKey=92496 and ID=688108 
+	Update [dbo].SMMHCInventoryLog	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ScoreTrakClientTargetMaster	SET ClientKey=100000 WHERE ClientKey=92496 and CreateDateTime>'2018-09-01'
+	Update [dbo].ClientStatusKids	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].Closure	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].DiagnosisUpdate SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].PNChartMaster	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientComms	SET ClientKey=100000 WHERE ClientKey=92496 
+	Update [dbo].ValeoInHouseCSCRef	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ClientPacketFormSignature	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ProgNoteValeoCrisis	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+	Update [dbo].ValeoMasterPSRef	SET ClientKey=100000 WHERE ClientKey=92496 and EnrollmentKey=688108 
+
+	EXEC CTSP_Core_Utility_RebuildClientCurrentDataByClient 92496
+
+        EXEC CTSP_Core_Utility_RebuildClientCurrentDataByClient 100000
 
 	COMMIT TRANSACTION [tr1];      
 		  SET TRANSACTION ISOLATION LEVEL READ COMMITTED;                
